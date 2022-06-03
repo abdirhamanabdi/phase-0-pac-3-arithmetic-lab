4ea9ccb83e78a2ae3eb259dd175907e21cda6bfc
Abdirahman Abdi <abdirahmanabdi.ma@gmail.com>
2022-06-03 22:59:11 +0300

    your message

---
+++ index.js
@@ -0,0 +1,54 @@
+function add(){
+
+}
+function subtract(){
+
+}
+function multiply(){
+    
+}
+function divide(){
+    
+}
+function add(a, b) {
+    return a + b;
+  }
+
+  function subtract(a, b) {
+    return a - b;
+  }
+  
+  function multiply(a, b) {
+    return a * b;
+  }
+  function divide(a, b) {
+    return a / b;
+  }
+  n = 10
+  function increment(n){
+      n++;
+      return n;
+  }
+
+  n = 10
+  function decrement(n){
+      n--;
+      return n;
+  }
+      n = 2
+ function makeInt(n){
+     parseInt('5.2',3)
+     return n
+ }
+ 
+
+n = '2.2'
+function preserveDecimal(n){
+parseFloat(n)
+return n
+}
+
+n = '2.2'
+function preserveInt(n){
+return n
+}
